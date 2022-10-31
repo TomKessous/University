639f6cf723068f19582e6ddaa48abffd9f786ce6
TomKessous <88094468+TomKessous@users.noreply.github.com>
2022-10-31 21:33:51 +0200

    Add files via upload

---
+++ Operating Systems/cse-os_Ex1_206018749_316333079/main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+pid_t my_fork(void);
+void print_pids(FILE* fd, short unsigned int N, short unsigned int G);
+void count_lines(short unsigned int G);
+
+
+
+int main(int argc, char *argv[]){
+    short unsigned int N=atoi(argv[1]),G=atoi(argv[2]);		//Converting input N,G to short unsigned int.
+    FILE *fd = fopen("out.txt","w" );
+    print_pids(fd,N, G);
+    fclose(fd);
+    count_lines(G);
+}
+
+pid_t my_fork (void){
+    pid_t pid;
+    if ((pid = fork()) > 0){
+        wait(NULL);
+        return pid;
+    }
+    else if(pid==0){
+        return pid;
+    }
+    else{
+        printf("Error occured while running a fork");
+        exit(1);
+    }
+}
+
+void print_pids(FILE* fd, short unsigned int N, short unsigned int G){
+    int n=0,g=0;
+    for(n=0;n<N;n++){
+        if(g<G){
+            if(my_fork() == 0){
+                n=-1;
+                g++;
+            }
+            else wait(NULL);
+        }
+        if(n==N-1) {
+            fprintf(fd, "My pid is %d. My generation is %d\n",getpid(),g);
+            if(g!=0) exit(0);
+        }
+    }
+}
+
+void count_lines(short unsigned int G){
+    int cntr=0,i=0,args=0,g=0;
+    FILE* fd = fopen("out.txt","r");
+    args = fscanf(fd,"%*10c%d%*19c%d.\n",&i,&g);
+    while(args==2){
+        if(g==G) cntr++;
+        args = fscanf(fd,"%*10c%d%*19c%d.\n",&i,&g);
+        if(args!=2){
+            printf("Number of lines by processes of generation %d is %d\n",G,cntr);
+            if(G>0) {
+                if (my_fork() > 0) wait(NULL);
+                else {
+                    cntr = 0;
+                    G--;
+                    rewind(fd);
+                    args = fscanf(fd, "%*10c%d%*19c%d.\n", &i, &g);
+                }
+            }
+        }
+    }
+    exit(0);
+}
